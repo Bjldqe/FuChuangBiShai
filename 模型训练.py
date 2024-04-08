fdef5406db98ae26d2e623b3a57c13ab3dfcb142
北极来的企鹅(Bjldqe) <116808658+Bjldqe@users.noreply.github.com>
2024-04-08 21:59:59 +0800

    Update 模型训练.py

--- 模型训练.py
+++ 模型训练.py
@@ -1,22 +1,24 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import tensorflow as tf
-from sklearn.metrics import confusion_matrix,classification_report #导入混淆矩阵函数 
-import matplotlib
-from keras.applications import resnet50,mobilenet_v2
+import os
+from keras.layers import Flatten, Dense, Dropout, Input
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.utils import to_categorical # 用于one-hot编码
+from keras import utils
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
 from tensorflow import keras
 import efficientnet.tfkeras as efn
-#输入图片的大小,需要与存储的图片大小一致
-height=128
-width=128
-#输入模型类别个数
-class_num = 7
-#测试集和测试集标签所在位置
-test_X='../tmp/data/X_test.npy'
-test_Y='../tmp/data/Y_test(label).npy'
+epochs=300
+height=128 
+width=128                                             
+class_num=7
+batch_size=32
+#训练集
+X_train=np.load('../tmp/data/X_train.npy')
+Y_train=np.load('../tmp/data/Y_train.npy')
 
-base_model = efn.EfficientNetB0(input_shape=[height,width,3], include_top=False,weights='imagenet')
+base_model = efn.EfficientNetB0(input_shape=[128,128,3], include_top=False,weights='imagenet')
 base_model.trainable =  True
 # 冻结前面的卷积层，训练最后10层
 for layers in base_model.layers[:-10]:
@@ -26,49 +28,30 @@ model = keras.Sequential([
     keras.layers.GlobalAveragePooling2D(),
     keras.layers.Dense(class_num,activation='softmax')
 ])
-model.load_weights('../tmp/save_weights/EfficientNetB0.h5')
-
-# 取消冻结模型的顶层
-base_model.trainable = True
-fine = 129
-for layer in base_model.layers[:fine]:
-    layer.trainable = False
-model = keras.Sequential([
-    base_model,
-    #keras.layers.Dropout(0.25),
-    keras.layers.GlobalAveragePooling2D(),
-    keras.layers.Dense(class_num,activation='softmax')
-])
-#导入模型权重文件
-model.load_weights('../tmp/save_weights/EfficientNetB0_wt.h5')
-
-#导入测试数据
-test_X=np.load(test_X)
-#导入测试类别标签
-test_Y_True=np.load(test_Y)
-
-#对测试集进行预测
-pre_result = model.predict_classes(test_X)
-#预测结果-真实结果
-Z=pre_result-test_Y_True
-#预测正确的结果/所有的预测值
-test_acc=len(Z[Z==0])/len(Z)
-print('预测准确率为：',test_acc)
-
-cm = confusion_matrix(test_Y_True, pre_result) #混淆矩阵   
-plt.matshow(cm, cmap=plt.cm.Greens) #画混淆矩阵图，配色风格使用cm.Greens，更多风格请参考官网。 
-plt.colorbar() #颜色标签  
-# 设置matplotlib正常显示中文和负号
-matplotlib.rcParams['font.sans-serif']=['SimHei']   # 用黑体显示中文
-matplotlib.rcParams['axes.unicode_minus']=False     # 正常显示负号
-for x in range(len(cm)): #数据标签  
-    for y in range(len(cm)):  
-        plt.annotate(cm[x,y], xy=(x, y), horizontalalignment='center', verticalalignment='center')  
-        plt.ylabel('True label') #坐标轴标签  
-        plt.xlabel('Predicted label') #坐标轴标签  
-#显示混淆矩阵可视化结果
-plt.savefig('../tmp/混淆矩阵.jpg',dpi=3090)
-plt.show()
-print(classification_report(test_Y_True,pre_result))
-
-
+#模型可视化1，使用model.summary()方法
+model.summary()
+model.compile(optimizer=tf.keras.optimizers.legacy.SGD(learning_rate=0.001,momentum=0.9, decay=1E-5),#优化器
+              loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),#损失率计算
+              metrics=["accuracy"])#监视器，打印准确率
+ # 创建保存模型的文件夹
+if not os.path.exists("../tmp/save_weights"):
+    os.makedirs("../tmp/save_weights")
+#保存模型，回调函数
+callbacks = [tf.keras.callbacks.ModelCheckpoint(filepath='../tmp/save_weights/EfficientNetB0.h5',#保存模型位置和命名
+                                                save_best_only=True,#保存最佳的参数
+                                                save_weights_only=False,#为True，则只保存模型权重，否则将保存整个模型（包括模型结构，配置信息等）
+                                                monitor='val_loss')]#监视验证集损失函数，判断最佳参数
+# 开始训练
+print("开始训练网络···")
+from sklearn.model_selection import KFold
+KF = KFold(n_splits =10)  #建立4折交叉验证方法 查一下KFold函数的参数
+for train1_index, val1_index in KF.split(X_train):
+    x_train1, y_train1 = X_train[train1_index], Y_train[train1_index]
+    x_val1, y_val1 = X_train[val1_index], Y_train[val1_index]
+history=model.fit(  x_train1,
+                    y_train1,  #训练集
+                    shuffle=True,#打乱数据
+                    steps_per_epoch=len(X_train) // batch_size,  #迭代频率
+                    epochs=epochs,  #迭代次数
+                    validation_data=(x_val1,y_val1),
+                    callbacks=callbacks)
